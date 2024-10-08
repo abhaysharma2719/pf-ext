01ca0cb4db242b125a74c29fbf9a0037efc855c6
abhaysharma2719 <abhay@techcrumb.com>
2024-10-08 20:24:56 +0530

    created the extension files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import path from 'path';  // Import path module
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 // Define __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -9,11 +10,22 @@ const __dirname = dirname(__filename);
 import { jsLoader, cssLoader } from './webpack/loaders.js';  // Import loaders
 
 export default {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'extension'),  // Correctly define output path
-        filename: 'index.js',
+    entry: {
+        popup: './src/popup.js',
+        content : './src/content.js'
     },
+    output: {
+        path: path.resolve(__dirname, 'extension'), // Correctly define output path
+        filename: '[name].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/static-files/popup.html', // Path to your HTML template
+            filename: 'popup.html', // Output HTML file name
+            scriptLoading: 'blocking', // Optional: load script in blocking mode
+            chunks: ['popup']
+        }),
+    ],
     module: {
         rules: [jsLoader, cssLoader],  // Use defined loaders
     },
